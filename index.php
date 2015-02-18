872ecf1429cdd8da25857a5fe325ee9442f5d745
Chris <cdiana.media@gmail.com>
2015-02-17 22:44:49 -0500

    NotORM database configuration

--- index.php
+++ index.php
@@ -7,3 +7,14 @@ require 'plugins/NotORM.php';
 /* Register autoloader and instantiate Slim */
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
+
+/* Database Configuration */
+$dbhost   = 'localhost';
+$dbuser   = 'root';
+$dbpass   = '';
+$dbname   = 'garages';
+$dbmethod = 'mysql:dbname=';
+
+$dsn = $dbmethod.$dbname;
+$pdo = new PDO($dsn, $dbuser, $dbpass);
+$db = new NotORM($pdo);
